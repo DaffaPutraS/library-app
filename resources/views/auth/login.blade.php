815a6d30cae5975cb905987c78dcdf1b00de93f1
DaffaPutraS <daffap434@gmail.com>
2025-07-18 10:50:33 +0700

    feat(UI): delete forgot password and remember me action

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,20 +24,8 @@
             @enderror
         </div>
 
-        <div class="form-check mb-3">
-            <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-            <label for="remember_me" class="form-check-label">Remember me</label>
-        </div>
-
         <div class="d-grid gap-2">
             <button type="submit" class="btn btn-primary">Log in</button>
-        </div>
-        <div class="mt-3 text-center">
-            @if (Route::has('password.request'))
-                <a class="text-decoration-underline text-muted" href="{{ route('password.request') }}">
-                    Forgot your password?
-                </a>
-            @endif
         </div>
         <div class="mt-4 text-center text-muted">
             Belum punya akun? <a href="{{ route('register') }}" class="text-primary fw-semibold text-decoration-underline">Daftar Sekarang</a>
